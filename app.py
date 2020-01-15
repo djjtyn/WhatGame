9f793e740411122e2e3c9bfd05cd080d31eaf195
djjtyn <djjtynan@gmail.com>
2020-01-15 21:43:47 +0000

    .gitignore and env.py files created. env.py file linking into main app.py file

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+  import env 
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = "game_review"
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/get_games')
+def get_games():
+    return render_template("games.html", games = mongo.db.games.find())
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',
