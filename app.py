8172e7442029e15abd4e02d7daf8f919a26afc4e
djjtyn <djjtynan@gmail.com>
2020-02-02 13:45:20 +0000

    comments added to python file

--- app.py
+++ app.py
@@ -18,20 +18,18 @@ mongo = PyMongo(app)
 @app.route('/')
 def all_games():
     return render_template ("all_games.html", games=list(mongo.db.games.find()))
+#Code above to render home page which shows all games listed on the site
     
-
 @app.route('/top_rated')
 def top_rated():
     return render_template ("top_rated.html", games=list(mongo.db.games.find().sort("rating", -1).limit(10)))
-
-@app.route('/view_games')
-def view_games():
-    return render_template("games.html", games = list(mongo.db.games.find()))
+#Code above to render top rated games page which shows the ten top most rated games on the site
 
 @app.route('/add_review')
 def add_review():
     return render_template("addreview.html", platforms = list(mongo.db.platforms.find()),
     ratings =list(mongo.db.rating.find()))
+#Code above to render page showing add a review form. platforms and ratings collections included in this form
 
 @app.route('/review_added', methods=["POST"])
 def review_added():
@@ -47,13 +45,9 @@ def review_added():
      'description': request.form.get('description'),
      'image':request.form.get('image')
     })
-    return redirect(url_for('add_confirmation'))
-#Code Above deals with form when initial game review is submitted and returns a confirmation for the review being added
-
-@app.route('/add_confirmation')
-def add_confirmation():
-   return render_template('addconfirmation.html')
-
+    return render_template('addconfirmation.html')
+#Code above adds values from the form into collection keys for new documents. Rating value is passed as an int and the 
+#rest are strings. After this new document is added a page showing a confirmation of the document addition is rendered.
 
 @app.route ('/edit_review/<game_id>')
 def edit_review(game_id):
@@ -62,6 +56,8 @@ def edit_review(game_id):
     all_ratings = mongo.db.rating.find()
     return render_template('editreview.html',game = game_review, 
     platforms = all_platforms, ratings = all_ratings )
+#Code above allows the user to click on an edit button for the games which brings them to an edit form similar to the add
+#review form. The only difference being that the form already has values reflecting the game pages information.
 
 @app.route ('/update_review/<game_id>', methods=["POST"])
 def update_review(game_id):
@@ -79,12 +75,14 @@ def update_review(game_id):
      'image':request.form.get('image')
     })
     return render_template('edit_review_confirmation.html',game=game_review)
+#Code above updates the values in the games key values reflecting the values inserted into the edit for game form. After the
+#form is sumbitted a page showing a confirmation that the edit has been sucessful is rendered
 
 @app.route('/delete_review/<game_id>')
 def delete_review(game_id):
     mongo.db.games.remove({'_id': ObjectId(game_id)})
     return redirect(url_for('all_games'))
-
+#Code above delets documents within the games collection. After the game is deleted the home page view is called upon.
 @app.route('/show_platform_form')
 def show_platform_form():
     return render_template('addplatform.html')    
@@ -95,18 +93,21 @@ def insert_platform():
     platform_doc = {'platform_name':request.form.get('platform_name')}
     platforms.insert_one(platform_doc)
     return render_template('newplatform.html')
+#Code above adds a platform which the user submits into the platform collection. After this addition is made a page showing
+#the addition s being sucessful is rendered
 
 @app.route ('/show_review/<game_id>')
 def show_review(game_id):
     game_review = mongo.db.games.find_one({"_id":ObjectId(game_id)})
     return render_template('game_page.html',game = game_review)
-    
+#Code above allows each game review to have it's own individual URL using the document's id key
+
 @app.route('/search')
 def search():
     query=request.args.get('search')
     all=mongo.db.games.find_one({"game_name":query})
     return render_template('search.html', all=all)
-
+#Code above is an attempt at enabling a search function for the game collection database. This is still a work in progress
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',
